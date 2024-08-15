c3aa06f8401ddc143bd2ee00fbb11bc0d6beb816
Bhuvnesh verma <99537126+MasterBhuvnesh@users.noreply.github.com>
2024-08-15 15:43:18 +0530

    made changes in timetable_A1_A2 data format

--- models/timetable_A1_A2.js
+++ models/timetable_A1_A2.js
@@ -1,28 +1,19 @@
-// const mongoose = require("mongoose");
-
-// const ClassesSchema = new mongoose.Schema({
-//   key: String,
-//   name: String,
-//   timeStart: String,
-//   timeEnd: String,
-//   teacher: String,
-//   room: String,
-// });
-
-// const TimetableSchema = new mongoose.Schema({
-//   day: String,
-//   classes: [ClassesSchema],
-// });
-
-// const TimetableModel_A1_A2 = mongoose.model("timetable_A1_A2", TimetableSchema);
-
-// module.exports = TimetableModel_A1_A2;
-
-// // ------------------------------------------------------------------
-
 const mongoose = require("mongoose");
 
-const TimetableSchema = new mongoose.Schema({}, { strict: false }); // Allows any structure
+const ClassesSchema = new mongoose.Schema({
+  key: String,
+  name: String,
+  timeStart: String,
+  timeEnd: String,
+  teacher: String,
+  room: String,
+});
+
+const TimetableSchema = new mongoose.Schema({
+  day: String,
+  classes: [ClassesSchema],
+});
+
 const TimetableModel_A1_A2 = mongoose.model("timetable_A1_A2", TimetableSchema);
 
 module.exports = TimetableModel_A1_A2;
